e5eec8347ee1e3b9e2658647b9dff3b4234a9e17
Prakash Khatua <103178764+PrakashSDE@users.noreply.github.com>
2022-08-20 22:40:52 +0530

    Add files via upload

---
+++ CallStack.js
@@ -0,0 +1,38 @@
+  //call stack 
+
+     display();
+
+  function display() {
+          console.log("My");
+          display1();         
+}
+
+function display1() {
+          console.log("Name");
+          display2();
+}
+
+function display2(){
+          console.log("is");
+          display3();
+}
+
+function display3(){
+          console.log("Prakash");
+          display4();
+}
+
+function display4(){
+          console.log("Khatua");
+
+}
+
+
+
+
+
+
+
+
+ 
+   
